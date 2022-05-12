3f3844debcb0222235e959179cb0b21e0d77bf3d
kaminibpatel <44550690+kaminibpatel@users.noreply.github.com>
2022-05-12 13:34:26 -0400

    FeedbackForm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Header from './components/Header'
 import index from './index.css'
 import FeedbackList from './components/FeedbackList'
+import FeedbackState from './components/FeedbackStats'
 import FeedbackData from './data/FeedbackData'
+import FeedbackForm from './components/FeedbackForm'
 import{useState} from 'react'
 
 function App(){
   const [feedback, setFeedback]=useState(FeedbackData)
   const feedbackDelete = (id) => {
-    if(window.confirm('Are you sure you wan to delete?'))
+    if(window.confirm('Are you sure you want to delete?'))
    setFeedback(feedback.filter((item)=> (item.id) !== id))
   }
 return (
   <> 
     <Header/>
     <div className = 'container'>
+      <FeedbackForm/>
+      <FeedbackState feedback={feedback}/>
      <FeedbackList feedback={feedback} handleDelete={feedbackDelete}/>
     </div>
   </>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,26 @@
+import Card from './shared/Card'
+import {useState} from 'react'
+
+function FeedbackForm() {
+    const [text, setText] = useState('')
+    const handleTextChange = (e) =>{
+        setText(e.target.value)
+    }
+
+    return (
+        <Card>
+            <form>
+                <h2>How would you rate your service with us?</h2>
+                <div className="input-group">
+                    <input type='text' 
+                    onChange={handleTextChange} 
+                    placeholder= 'Write your review'
+                    value={text}/>
+                    <button type="submit" >Send</button>
+                </div>
+            </form>
+        </Card>
+    )
+}
+
+export default FeedbackForm
